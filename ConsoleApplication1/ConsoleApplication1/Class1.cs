14ae2992c88761b783b73fc6cef54e8a7f1cde5f
AlbertoAmaya77 <47874784+AlbertoAmaya77@users.noreply.github.com>
2019-03-19 19:51:23 -0600

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        int num1, num2, result;
+
+
+            public void Multiplicar()
+            {
+                    Console.WriteLine("Por favor ingrese el primer valor a multiplicar: ");
+                    num1 = int.Parse(Console.ReadLine());
+
+                    if (num1==0)
+                    {
+                        Console.WriteLine("El producto de 0 por cualquier numero es 0.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Por favor ingrese el segundo valor a multiplicar: ");
+                        num2 = int.Parse(Console.ReadLine());
+
+                        result = num1 * num2;
+
+                        Console.WriteLine("El resultado de la multiplicacion es: " + result);
+                    }
+             }
+            public void Dividir()
+            {
+                    Console.WriteLine("Por favor ingrese el primer valor a dividir: ");
+                    num1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Por favor ingrese el segundo valor a dividir: ");
+                    num1 = int.Parse(Console.ReadLine());
+
+                    if (num2==0)
+                    {
+                        Console.WriteLine("Error: No se puede dividir entre cero");
+                    }
+                    else
+                    {
+                        result = num1/num2;
+
+                        Console.WriteLine("El resultado de la division es: " + result);
+                    }
+             }
+
+            public void Nombres()
+            {
+                String [5] nom = new String [5];
+                int i;
+                i = 1;
+                for (i <= 5; i++)
+                {
+                    Console.WriteLine("Por favor ingrese el nombre: ");
+                    nom[i] = Console.ReadLine();
+                }
+
+                for (i <= 5; i++)
+                {
+                    Console.WriteLine("El nombre es: " + nom[i]);
+                }
+            }
+
+            public Edades()
+            {
+                int [5] edades = new int [5];
+                int i;
+                i = 1;
+                while (i <= 5)
+                {
+                    Console.WriteLine("Por favor ingrese la edad ");
+                    edades[i] = Console.ReadLine();
+
+                    i = i+1;
+                }
+
+                while (i <= 5)
+                {
+                    Console.WriteLine("La edad es: " + edades[i]);
+
+                    i = i+1;
+                }
+            }
+
+     }   
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+
+            Class1 obj1 = new obj1();
+            Class1 obj2 = new obj2();
+            Class1 obj3 = new obj3();
+            Class1 obj4 = new obj4();
+
+            Console.WriteLine("***MENU DE OPCIONES***");
+            Console.WriteLine("Para multiplicar ingrese 1");
+            Console.WriteLine("Para dividir ingrese 2");
+            Console.WriteLine("Para ingresar nombres digite 3");
+            Console.WriteLine("Para ingresar edades digite 4");
+            opcion = int.Parse(Console.ReadLine());
+
+            switch:
+            {
+                case 1:
+                    obj1.Multiplicar();
+
+                    break;
+
+                case 2:
+                    obj2.Dividir();
+
+                    break;
+
+                case 3:
+                    obj3.Nombres()
+
+                    break;
+
+                case 4:
+                    obj3.Edades();
+
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
